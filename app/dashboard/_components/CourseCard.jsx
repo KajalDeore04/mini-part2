115ec8072a08a32bcf9e21e740c4acaad259919e
KajalDeore04 <kajaldeore04@gmail.com>
2025-02-17 12:32:04 +0530

    course list on dashboard

---
+++ app/dashboard/_components/CourseCard.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import React from 'react';
+import { HiOutlineBookOpen } from 'react-icons/hi2';
+
+const CourseCard = ({course}) => {
+    return (
+        <div className='shadow-lg p-2 rounded-lg border hover:scale-105 transition-all cursor-pointer mt-4' >
+             <Image src={'/placeholder.png'} width={300} height={300}
+             className='w-full h-[200px] object-cover rounded-lg' />
+             <div className='p-2'>
+                <h2 className='font-medium text-lg'>{course?.courseOutput?.courseName}</h2>
+
+                <p className='text-sm text-gray-400 my-2'>{course?.courseOutput?.category}</p>
+
+            <div className='flex items-center justify-between'>
+                <h2 className='flex gap-2 items-center p-1 bg-purple-50 text-primary text-sm rounded-sm'><HiOutlineBookOpen/> 
+                {course?.courseOutput?.noOfChapters} Chapters</h2>
+
+
+                <h2 className='text-sm bg-purple-50 text-primary p-1 rounded-sm'>{course?.courseOutput?.level}</h2>
+            </div>
+             </div>
+        </div>
+    );
+}
+
+export default CourseCard;
