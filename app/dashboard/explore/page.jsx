260d6643f0341dc9aeeb76be49537cf8daa1da0b
KajalDeore04 <kajaldeore04@gmail.com>
2025-02-18 14:52:19 +0530

    explore page

--- app/dashboard/explore/page.jsx
+++ app/dashboard/explore/page.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+"use client";
+import { db } from "@/configs/db";
+import { CourseList } from "@/configs/schema";
+import React, { useEffect, useState } from "react";
+import CourseCard from "../_components/CourseCard";
+import { Button } from "@/components/ui/button";
 
 const Explore = () => {
-    return (
-        <div>
-            explore
-        </div>
-    );
-}
+  const [courseList, setCourseList] = useState([]);
+  const [pageIndex, setPageIndex ] = useState(0)
+
+  useEffect(() => {
+    GetAllCourses();
+  }, [pageIndex]);
+
+  const GetAllCourses = async () => {
+    const result = await db.select().from(CourseList).limit(9).offset(pageIndex*9);
+
+    console.log(result);
+    setCourseList(result);
+  };
+  return (
+    <div>
+      <h2 className="font-bold text-3xl">Explore More Projects</h2>
+      <p className="">Explore projects built using AI by other users</p>
+
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-5">
+        {courseList.map((course, index) => (
+          <div>
+            <CourseCard course={course} displayUser={true} />
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-between mt-5">
+        {pageIndex !=0 && <Button onClick={()=>setPageIndex(pageIndex-1)}>Previous Page</Button>}
+        <Button className=' ' onClick={()=>setPageIndex(pageIndex+1)}>Next Page</Button>
+      </div>
+    </div>
+  );
+};
 
 export default Explore;
